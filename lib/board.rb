6a72d88c59aca2b14243db202708f86e5498d86a
blpeters <brett.peters@utexas.edu>
2023-01-26 12:40:21 -0600

    board display unit tests

--- lib/board.rb
+++ lib/board.rb
@@ -4,7 +4,30 @@ class Board
   
   attr_reader :table
 
-  def initialize
-    @table = Array.new(6) { Array.new(7) }
+  def initialize(table = Array.new(6) { Array.new(7) })
+    @table = table
+  end
+
+  def display
+    output = ''
+    table.each do |row|
+      row.each do |slot|
+        case slot
+        when nil
+          output += "\u26aa"
+        when 1
+          output += "\u26ab"
+        when 2
+          output += "\u26ab"
+        end
+      end
+      output += "\n"
+    end
+    puts output
+    output
   end
 end
+
+# board = Board.new([[nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [1, nil, nil, 2, nil, nil, nil]]) 
+
+# board.display
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,10 +3,12 @@
 require_relative '../lib/board'
 
 describe Board do
+
+  subject(:board) { described_class.new }
+
   describe '#initialize' do
     context 'when a new game begins' do
 
-      subject(:board) { described_class.new }
 
       it 'creates a new empty 6 row, 7 columns, two-dimensional array' do
         expect(board.table).to eq([[nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil]])
@@ -14,5 +16,27 @@ describe Board do
     end 
   end
 
-  
+  describe '#display' do
+    context 'when the board is empty' do
+      it 'displays board'do
+      output = "\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n"
+      expect(board.display).to eq(output)
+      end
+    end
+
+    context 'when the board is populated' do
+      
+      populated_table = [[nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil], [1, nil, nil, 2, nil, nil, nil]]
+      subject(:populated_board) { described_class.new(populated_table) }
+      
+      it 'displays board' do
+        output = "\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\u26aa\n\u26ab\u26aa\u26aa\u26ab\u26aa\u26aa\u26aa\n"
+        expect(populated_board.display).to eq(output)
+      end
+    end
+
+  end
+
+
+
 end
